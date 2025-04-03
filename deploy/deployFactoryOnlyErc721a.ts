47139cc0bab12fef4874687ffaaf762d28170d74
kneelsdev <sussykneelsnft@protonmail.com>
2025-04-03 00:15:07 -0400

    factory script

--- deploy/deployFactoryOnlyErc721a.ts
+++ deploy/deployFactoryOnlyErc721a.ts
@@ -3,16 +3,19 @@ import { ethers, upgrades, run } from "hardhat";
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main() {
-    const archetypeAddress = '0x1a357b2246aa7A620A0d4F044e28CDC4f10800F8'
+    const archetypeAddress = '0x3C24Cd3CB12408C0f9350917ad4d85496f2D68e1'
     console.log("Archetype deployed to:", archetypeAddress);
 
-    const Factory = await ethers.getContractFactory("FactoryZksyErc721a");
+    const Factory = await ethers.getContractFactory("FactoryZksyncErc721a");
 
-    const factory = await Factory.deploy(archetypeAddress);
+    // const factory = await Factory.deploy(archetypeAddress);
 
-    const factoryAddress = await factory.getAddress();
+    // const factoryAddress = await factory.getAddress();
 
-    console.log("Contract Factory deployed to:", factoryAddress);
+    const factory = await Factory.attach('0x38A781410E4ba53899E3A19207C4c02DC8b9c18A')
+
+    const result = await factory.setDeployFee(BigInt(1500000000000000))
+    console.log("Contract Factory deployed to:", result);
 }
 
 main()
